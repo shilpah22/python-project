6cff6d3bfd5c76311afaa0d95743d81176c2092e
shilpah22 <havalada.shilpa@gmail.com>
2025-06-26 16:25:23 +0100

    Ignore temp.txt

--- script.py
+++
@@ -1,6 +0,0 @@
-print("Hello World")
-name=input("Enter your name:")
-id=input("Enter your college id:")
-print("Student name is {} and id is {}".format(name,id))
-import datetime
-print("Today's date is:", datetime.date.today())
